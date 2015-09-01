d177180de1899bf243389e4369e7905b0d0a9866
William Hammond <wth4280@g.rit.edu>
2015-09-01 16:25:14 -0400

    Added forward prediction model

--- nnet_function.R
+++ nnet_function.R
@@ -1,12 +1,12 @@
 library(nnet)
 library(neuralnet)
-library(clusterSim)
+# library(clusterSim)
 
 
 ### Read in Result Data ###
 results_N <- read.table("Results/results_normal.txt")
 results_HF <- read.table("Results/results_hf.txt")
-distro_N <- read.table("Reslts/distro_normal.txt")
+distro_N <- read.table("Results/distro_normal.txt")
 distro_HF <- read.table("Results/distro_HF.txt")
 
 features <- do.call(rbind, list(results_N,results_HF))
@@ -49,22 +49,38 @@ train <- training[sample(nrow(training)),]
 testing <- cbind(test_input, test_output)
 test <- testing[sample(nrow(testing)),]
 
-
 # Build your Artificial Neural Network Model
-# model_nnet <- nnet(x=train[,11:26], hidden = 1000000000000000, 
-#                    y=train[,1:10], size=35,maxit = 10000, linout = TRUE, 
-#                    trace = TRUE)
+model_nnet <- nnet(x=train[,11:26], hidden = 1000000000000000, 
+                   y=train[,1:10], size=35,maxit = 10000, linout = TRUE, 
+                   trace = TRUE)
 x=train[,11:26]
 y=train[,1:10]
-model_nnet <- neuralnet(y[,1]~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+
-                          V13+V14+V15+V16,x,hidden = 10, err.fct = 'sse',
-                          rep = 2)
-plot(compute(model_nnet, x[,1:16])$net.result)
+# model_nnet <- neuralnet(y[,1]~V1+V2+V3+V4+V5+V6+V7+V8+V9+V10+V11+V12+
+#                           V13+V14+V15+V16,x,hidden = 10, err.fct = 'sse',
+#                           rep = 2)
+# plot(compute(model_nnet, x[,1:16])$net.result)
 
 # Use the model for prediction pruposes
 predicted=predict(model_nnet, test_output, type = "raw")
 
-
 accuracy=((predicted-test_input)/test_input)*100
 
 test <- as.data.frame(cbind(test_input[,1],predicted[,1],seq(1,100,1)))
+
+# Forward Model
+target = train[,11:26]
+n.col = 16
+# forward_predicted = list()
+# forward_accuracy = list()
+for (i in 1:n.col){
+  title <- paste('Forward Model',i,sep='')
+  forward_nnet <- nnet(x=train[,1:10], hidden = 1000000000000000, 
+                       y=target[,i], size=35,maxit = 10000, linout = TRUE, 
+                       trace = TRUE)
+  forward_predicted=predict(forward_nnet, test_input, type = "raw")
+  forward_accuracy= ((forward_predicted-test_output[,i])/test_output[,i])*100
+  
+  jpeg (paste(title,'.jpeg', sep =''))
+  plot(forward_accuracy)
+  dev.off()
+}
